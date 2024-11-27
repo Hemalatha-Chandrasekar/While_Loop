694b7871fb4ebeafc64bf2c62104eae17b8b9713
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-11-26 22:12:09 -0600

    Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+    While_Loops WL = new While_Loops();
+    WL.GuesstheNumber();
+    WL.SubtractionQuizLoop();
+    WL.SubtractionQuizLoop();
+    WL.SentinelValuedemo();
+    }
+}
